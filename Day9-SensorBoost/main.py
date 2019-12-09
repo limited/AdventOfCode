a5d83dc9f1b3650ae23b2d4c9e02d37d1c164ea6
efriedrich <eric.friedrich@disneystreaming.com>
2019-12-09 10:30:08 -0500

    Day9 done

--- Day9-SensorBoost/main.py
+++ Day9-SensorBoost/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-# Start 8:38AM, ugh more intcode
+# Start 8:38AM, ugh more intcode, needed some refactoring
+# Done 10:30AM
+#      --------Part 1--------   --------Part 2--------
+#Day       Time   Rank  Score       Time   Rank  Score
+#  9   10:24:34   6436      0   10:28:34   6367      0
 
 def read_input(name):
     init_mem = [0 for x in range(0,5000000)]
@@ -28,35 +32,35 @@ def execute(mem, pc=0):
                     4: 1, 5: 2, 6: 2,
                     7: 3, 8: 3, 9: 1}
         
-        print(f"c:{count} pc:{pc} o:{opcode} p:{param_mode} {mem[pc+1]} {mem[pc+2]} {mem[pc+3]}")
+        #print(f"c:{count} pc:{pc} o:{opcode} p:{param_mode} {mem[pc+1]} {mem[pc+2]} {mem[pc+3]}")
         
         r = mem[pc+1 : pc+4]
-        print(f"regs: {r}")
+        #print(f"regs: {r}")
         
         for x in range(2, 2-inst_len[opcode], -1):
             if param_mode[x] == "0":
-                print(f"--{x} positional mode, reading @{r[2-x]}")
+                #print(f"--{x} positional mode, reading @{r[2-x]}")
                 r[2-x] = mem[r[2-x]]
                 
             elif param_mode[x] == "1":
-                print(f"--{x} immediate mode, using {r[2-x]}")
+                #print(f"--{x} immediate mode, using {r[2-x]}")
                 pass
             
             elif param_mode[x] == "2":
-                print(f"--{x} relative mode, reading @{r[2-x]}+{relative_base}")
+                #print(f"--{x} relative mode, reading @{r[2-x]}+{relative_base}")
                 r[2-x] = mem[r[2-x]+relative_base]
 
-        print(f"regs: {r}")
+        #print(f"regs: {r}")
                     
         if param_mode[0] == "1":
             print("ERROR, immediate mode for output")
         
         if opcode == 1: # ADD Opcode
             if param_mode[0] == "2":
-                print(f"storing {r[0]} + {r[1]} to {mem[pc+3]}+{relative_base}")
+                #print(f"storing {r[0]} + {r[1]} to {mem[pc+3]}+{relative_base}")
                 mem[mem[pc+3]+relative_base] = r[0] + r[1]                
             else:
-                print(f"storing {r[0]} + {r[1]} to {mem[pc+3]}")
+                #print(f"storing {r[0]} + {r[1]} to {mem[pc+3]}")
                 mem[mem[pc+3]] = r[0] + r[1]
 
         elif opcode == 2: # MULT Opcode
