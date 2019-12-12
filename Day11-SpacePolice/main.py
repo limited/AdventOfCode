900d666b23b09218e3a58954984e523eeeb4b7df
efriedrich <eric.friedrich@disneystreaming.com>
2019-12-12 10:18:36 -0500

    Day12 Part2, needed hints

--- Day11-SpacePolice/main.py
+++ Day11-SpacePolice/main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-# Start 8:00
+# Start 8:00, snowblowed until 9ish
+# Part1: 9:27
+# Part2: 10:45, wasted about an hour figuring out that I was rendering all pixels as white regardless of value
 
-move_dict = { 'u': (0,1),
-              'd': (0,-1),
+
+
+move_dict = { 'u': (0, 1),
+              'd': (0, -1),
               'l': (-1, 0),
-              'r': (1, 0)
+              'r': (1,  0)
               }
+
 next_face = {'u': {0: 'l', 1: 'r'},
              'd': {0: 'r', 1: 'l'},
              'l': {0: 'd', 1: 'u'},
@@ -28,17 +33,16 @@ def input_func(hull, pos):
         return hull[pos]
     else:
         return 0
-        
-def execute(mem, hull, pos, pc=0):
+
+def execute(mem, hull, pos, pc=0, relative_base = 0):
     count = 0
-    relative_base = 0
     
     while True:
         count += 1
         
         opcode = mem[pc] % 100
         if opcode == 99:
-            return (True, 0, 0)
+            return (True, None, pc, relative_base)
         
         param_mode = str(mem[pc]).zfill(5)
         inst_len = {1: 3, 2: 3, 3: 1,
@@ -96,7 +100,7 @@ def execute(mem, hull, pos, pc=0):
         elif opcode == 4:
             #print(f"Outputting {r[0]}")
             #print(r[0])
-            return (False, r[0], pc+2)
+            return (False, r[0], pc+2, relative_base)
             break
 
         elif opcode == 5:
@@ -107,7 +111,7 @@ def execute(mem, hull, pos, pc=0):
             
         elif opcode == 6:
             if (r[0] == 0):
-                #print(f"jumping to {r2}")
+                #print(f"jumping to {r[1]}")
                 pc = r[1]
                 continue
 
@@ -140,24 +144,51 @@ def main():
     pos = (0,0)
     hull = dict()
     pc = 0
+    rb = 0
 
-    facing = 'u'
-    while True:
-        (halt, color, pc) =  execute(mem, hull, pos, pc)
-        hull[pos] = color
-        #print(f"Painting {pos} to {color}")
+    # Emergency Hull Painting Robot Starting Panel
+    hull[pos] = 1
+
+    facing = 'l'
+    try:
+        while True:
+            (halt, color, pc, rb) =  execute(mem, hull, pos, pc, rb)
+            if halt:
+                print("--------HALT---------")
+                break
+            hull[pos] = color
+            print(f"Painting {pos} to {color}")
         
-        (halt, turn, pc) = execute(mem, hull, pos, pc)
-        if halt:
-            print(f"Painted {len(hull.keys())} panels")
-            break
+            (halt, turn, pc, rb) = execute(mem, hull, pos, pc, rb)
+            if halt:
+                
+                print(f"Painted {len(hull.keys())} panels")
+                break
 
-        #print(f"Was facing {facing}")
-        facing = next_face[facing][turn]
-        #print(f"Turning {turn} now facing {facing} at {pos}" )
-        pos = (pos[0] + move_dict[facing][0] ,pos[1]+move_dict[facing][1])
-        #print(f"After move at {pos}")
-        #print("-----")
+            #print(f"Was facing {facing}")
+            facing = next_face[facing][turn]
+            print(f"Turning {turn} now facing {facing}")# at {pos}" )
+            pos = (pos[0] + move_dict[facing][0] ,pos[1]+move_dict[facing][1])
+            print(f"Moving to {pos}")
+            #print("-----")
+                      
+    except KeyError:
+        print("KeyError")
+        return
+
+    print(len(hull))
+    print(hull)
+        
+    size = 256
+    from PIL import Image
+    img = Image.new('RGB', (size, size), "black")
+    pixels = img.load()
+    center = size/2
+    print(type(hull))
+    for p in hull:
+        if hull[p] == 1:    
+            pixels[p[0]+center, p[1]+center] = (255, 255, 255)
+    img.show()
 
 if __name__ == '__main__':
     main()
--- Day12-NBodyProblem/main.py
+++ Day12-NBodyProblem/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+# Needed hints on
+# a) cycle w/ init position
+# b) using LCM of indepdent axis cycle lengths
 
 import re
 import itertools
+import numpy
 
 #infile='test1'
 infile='game_input'
@@ -15,11 +19,19 @@ for l in f:
     if match is not None:
         pos.append([int(match.group(1)), int(match.group(2)), int(match.group(3))])
         vel.append([0,0,0])
-print(pos)
 
-steps = 1000
-for s in range(0, steps):
-    #XXX_EF Gravity
+
+init_state = list()
+cycle_length = [0,0,0]
+for d in range(0,3):
+    init_state.append([body[d] for body in pos])
+    init_state[d].extend([body[d] for body in vel])
+print(init_state)
+
+count = 0
+run = True
+while run:
+    # Gravity
     for pair in itertools.combinations(range(0, len(pos)), 2):
         #print(pair)
         for d in range(0, 3):
@@ -42,10 +54,33 @@ for s in range(0, steps):
         for d in range(0,3):
             pos[i][d] = pos[i][d] + vel[i][d]
 
+    count += 1
+    
+    # Check cycle in any axis
+    # XXX_EF Might need to include velocity in state check
+    for d in range(0,3):
+        axis_state = [body[d] for body in pos]
+        axis_state.extend([body[d] for body in vel])
+        if cycle_length[d] == 0 and axis_state == init_state[d]:
+            print(f"Found cycle in axis {d} at step {count}")
+            cycle_length[d] = count
+            if 0 not in cycle_length:
+                run = False
+
+
+
     #for i in range(0, len(pos)):
     #    print(f"pos=<{pos[i][0]}, {pos[i][1]}, {pos[i][2]}>, vel=<{vel[i][0]}, {vel[i][1]}, {vel[i][2]}>")
-    
-    
+        
 energies = [sum(map(abs, pos[i])) * sum(map(abs, vel[i])) for i in range(0,len(pos))]
 print(f"Total Energy: {sum(energies)}")
+
+print(cycle_length)
+print(numpy.lcm.reduce(cycle_length))
+# 43156306344996 too low (had count in wrong spot)
+# 43156306344996 too low
+# 420788524631496 !!!
+
+#6732500091353405 too high (doing velocity state check wrong)
+
 
