3864688c71d3a4474892c8a5020f9b2aecbfcb22
efriedrich <eric.friedrich@disneystreaming.com>
2020-12-04 07:15:23 -0500

    Day3

--- 2020/Day3/main.py
+++ 2020/Day3/main.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
-
 with open('game_input', 'r') as f:
     trees = [l.rstrip() for l in f]
-
-        
-print(trees)        
         
 width = len(trees[0])
 
 def check_slope(right, down):
-    print(right, down)
     x = 0
     count = 0
 
     for (ridx, row) in enumerate(trees):
         if (ridx % down != 0):
             continue
-        #print(row)
-        #print(x, row[x] == '#')
         if row[x] == '#':
             count += 1
             
